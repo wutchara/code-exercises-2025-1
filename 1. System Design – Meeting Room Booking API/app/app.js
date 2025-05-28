8844d6faa416dd69054408120660fb9033c04302
wutchara <SSTMUHAM@gmail.com>
2025-05-28 22:37:35 +0700

    add logic to validate reserve body request

--- 1. System Design – Meeting Room Booking API/app/app.js
+++ 1. System Design – Meeting Room Booking API/app/app.js
@@ -27,6 +27,28 @@ app.get("/", (req, res) => {
   res.send("Hello World from Express!");
 });
 
+async function getRoomById(id) {
+  try {
+    // Use pool.execute() for prepared statements (recommended for security)
+    // or pool.query() for simple queries (less secure if not careful with user input)
+    const [rows] = await dbPool.execute("SELECT * FROM rooms WHERE id = ?", [
+      id,
+    ]);
+
+    if (rows.length === 0) {
+      return res.status(404).json({
+        status: Status.ERROR,
+        message: "Room not found",
+      });
+    }
+
+    return rows[0];
+  } catch (error) {
+    console.error("Error fetching rooms:", error);
+    throw error;
+  }
+}
+
 // return all aviable rooms
 app.get("/rooms", async (req, res) => {
   const id = req.query.id;
@@ -55,22 +77,9 @@ app.get("/rooms", async (req, res) => {
   } else {
     // show only the room with the given id
     try {
-      // Use pool.execute() for prepared statements (recommended for security)
-      // or pool.query() for simple queries (less secure if not careful with user input)
-      const [rows] = await dbPool.execute("SELECT * FROM rooms WHERE id = ?", [
-        id,
-      ]);
-
-      if (rows.length === 0) {
-        return res.status(404).json({
-          status: Status.ERROR,
-          message: "Room not found",
-        });
-      }
-
       result = {
         status: Status.SUCCESS,
-        data: rows[0],
+        data: getRoomById(id),
       };
     } catch (error) {
       console.error("Error fetching rooms:", error);
@@ -85,12 +94,89 @@ app.get("/rooms", async (req, res) => {
   res.json(result);
 });
 
-app.post("/reserve", (req, res) => {
-  const result = {
-    status: "success",
-    reservation_ids: ["resv_1", "resv_2", "resv_3", "resv_4", "resv_5"],
+// Define the enum using a plain object
+const RecurringType = Object.freeze({
+  NONE: "none",
+  DAILY: "daily",
+  WEEKLY: "weekly",
+  MONTHLY: "monthly",
+  YEARLY: "yearly",
+});
+
+app.post("/reserve", async (req, res) => {
+  let result = {
+    status: Status.ERROR,
+    message: "Invalid request",
   };
+
+  const roomId = req.body.room_id;
+
+  let useDate = false;
+
+  //   {
+  //   "room_id": "A101",
+  //   "start_time": "2025-06-01T09:00:00Z",
+  //   "end_time": "2025-06-01T10:00:00Z",
+  //   "recurring": {
+  //     "type": "weekly",
+  //     "day_of_week": "Monday",
+  //     "count": 5
+  //   }
+  // }
+  const body = req.body;
+  console.log("body", body);
+
+  let startDate;
+  let endDate;
+
+  if (body.startDate && body.endDate) {
+    useDate = true;
+    startDate = new Date(body.startDate);
+    endDate = new Date(body.endDate);
+    console.log("startDate", startDate);
+    console.log("endDate", endDate);
+  } else {
+    // TODO:
+    useDate = false;
+
+    // find startDate and endDate from body.recurring
+    const recurring = body.recurring;
+  }
+
+  // validate startDate and endDate
+  if (startDate >= endDate) {
+    result.message = "Invalid date range";
+  } else {
+    // validate roomId from request and database
+    try {
+      const room = getRoomById(roomId);
+      if (!room) {
+        result.message = "Invalid room id";
+      }
+
+      try {
+        // insert data into bookings database
+        const [rows] = await dbPool.execute(
+          "INSERT INTO bookings (room_id, user_id, start_time, end_time) VALUES (?, ?, ?, ?)",
+          [roomId, 1, startDate, endDate]
+        );
+        console.log("rows", rows);
+
+        result = {
+          status: Status.SUCCESS,
+          reservation_ids: [rows.insertId],
+        };
+      } catch (error) {
+        console.error("Error fetching rooms:", error);
+        result.message = error.message;
+      }
+    } catch (error) {
+      result.message = "Invalid room id";
+    }
+  }
+
   res.json(result);
+  return;
 });
 
 app.delete("/reserve", (req, res) => {
